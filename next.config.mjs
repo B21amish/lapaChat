5d123cf2ea8724c4b527e2e74a5b4d6eaec7faa4
thepmsquare <impmmmm@gmail.com>
2024-06-12 19:59:07 +0530

    🐛 build errors in typescript

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,26 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  webpack: (config, { isServer }) => {
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.resolve.fallback = { fs: false };
+    }
+
+    config.module.rules.push({
+      test: /\.tsx?$/,
+      use: [
+        {
+          loader: "ts-loader",
+          options: {
+            transpileOnly: true,
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
